d629798bad1717d26fe9f48ef5fca824fc796ebf
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-25 14:58:15 -0500

    filled out room card slots for table

--- client/src/components/tableUI/Table.jsx
+++ client/src/components/tableUI/Table.jsx
@@ -2,8 +2,7 @@ export default function Table() {
   return (
     <main className="bg-green-500 w-full rounded-md flex flex-col justify-between  items-center">
       <section className="flex justify-evenly gap-2 mt-10 w-5/6 h-20">
-  
-      <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
+        <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
         <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
 
         <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
