ed2f4e4552ca4e8bb899f1a2f6bbf8b3cf0a9d22
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-28 14:19:53 -0500

    added account form

--- client/src/components/generalUI/Account.jsx
+++ client/src/components/generalUI/Account.jsx
@@ -1,8 +1,26 @@
+import { useSelector } from "react-redux";
 
 export default function Account() {
+  const user = useSelector((state) => state.auth.user);
   return (
-    <div>
-      This is account access
+    <div className="card card-side bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="card-title">Account</h2>
+        <p>Current Balance: ${user.accountBalance}</p>
+        <div className="card-actions justify-end">
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Add Funds Here</span>
+            </div>
+            <input
+              type="text"
+              placeholder="$"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </label>
+          <button className="btn btn-success">Success</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
