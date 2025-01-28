beb9e0f2027e4a7c55f30aa5ecf12fc6e75608e3
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-01-28 13:43:01 -0500

    add placeholder for bet and chat

--- client/src/components/tableUI/Room.jsx
+++ client/src/components/tableUI/Room.jsx
@@ -1,27 +1,34 @@
 import { useParams } from "react-router";
 import Table from "./Table";
+import Seat from "./Seat";
 
 export default function Room() {
   let { roomId } = useParams();
   console.log("ROOM ID", roomId);
   return (
-    <main className="w-full min-h-screen flex flex-col justify-center bg-slate-400 border ">
+    <main className="w-full min-h-screen flex flex-col justify-center bg-slate-200  ">
       <section className="flex flex-col justify-center  items-center gap-2 w-full h-[70vh] bg-blue-700">
         {/* top */}
         <div className="flex gap-10 h-1/3  w-1/2 items-center justify-center">
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
+          <Seat />
+          <Seat />
         </div>
         {/* mid */}
         <div className="flex gap-5 w-full h-1/3  justify-center  text-center px-4">
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
+          <Seat />
           <Table />
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
+          <Seat />
         </div>
         {/* btm */}
         <div className="flex gap-10 h-1/3 w-1/2 items-center justify-center">
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
-          <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>
+          <Seat />
+          <Seat />
+        </div>
+      </section>
+      <section>
+        <div className="flex justify-between">
+          <p className="bg-green-200 w-1/2">BetBox</p>
+          <p className="bg-green-500 w-1/2">chatbox</p>
         </div>
       </section>
     </main>
---
+++ client/src/components/tableUI/Seat.jsx
@@ -0,0 +1,3 @@
+export default function Seat() {
+  return <p className="bg-white rounded-full w-1/4 h-5/6 text-center">s</p>;
+}
