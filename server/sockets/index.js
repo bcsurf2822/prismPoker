0a1e4ffd57fcd54fcc7a3c2f3dd6175d3ed5e5d0
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-02-04 12:14:55 -0500

    initial commit, add boilerplate for leave game socket

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -1,12 +1,12 @@
-
-
-const handlePlayerJoin = require("./room/joinGameSocket")
+const handlePlayerJoin = require("./room/joinGameSocket");
+const handlePlayerLeave = require("./room/leaveGameSocket");
 
 function setupSockets(io) {
   io.on("connection", (socket) => {
     console.log("a user connected");
 
-    handlePlayerJoin(io, socket)
+    handlePlayerJoin(io, socket);
+    handlePlayerLeave(io, socket);
 
     socket.on("disconnect", () => {
       console.log("user disconnected");
---
+++ server/sockets/room/leaveGameSocket.js
@@ -0,0 +1,48 @@
+const User = require("../../models/users");
+const Game = require("../../models/games");
+
+const handlePlayerLeave = (io, socket) => {
+  socket.on("playerLeave", async ({ gameId, userId, seatId }) => {
+    try {
+      const game = await Game.findById(gameId);
+      const user = await User.findById(userId);
+
+      if (!game || !user) {
+        return socket.emit("joinError", { message: "Game or User not found" });
+      }
+
+      if (user.accountBalance < buyIn) {
+        return socket.emit("joinError", { message: "Insufficient funds" });
+      }
+
+      const seat = game.seats.find(
+        (seat) => seat._id.toString() === seatId.toString()
+      );
+      if (!seat || seat.player) {
+        return socket.emit("joinError", {
+          message: "Seat is already occupied",
+        });
+      }
+
+      user.accountBalance -= buyIn;
+      await user.save();
+
+      seat.player = {
+        user: user._id,
+        chips: buyIn,
+        bet: 0,
+        action: "none",
+      };
+
+      await game.save();
+
+      io.to(gameId).emit("gameUpdated", game);
+      socket.emit("joinSuccess", { message: "Joined successfully", game });
+    } catch (error) {
+      console.error("Error joining game:", error);
+      socket.emit("joinError", { message: "Server error" });
+    }
+  });
+};
+
+module.exports = handlePlayerLeave;
