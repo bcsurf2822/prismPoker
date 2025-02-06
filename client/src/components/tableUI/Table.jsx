567655b00ef866fb1d7dd7d8ef163303326730c1
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-02-06 14:23:42 -0500

    fixed up chat/bet area and height of each section iin the table

--- client/src/components/tableUI/Table.jsx
+++ client/src/components/tableUI/Table.jsx
@@ -4,7 +4,7 @@ export default function Table({ pot }) {
   return (
     <main className="bg-green-600 w-full rounded-md flex flex-col justify-between  items-center">
             <section>$ {pot}</section>
-      <section className="flex justify-evenly gap-2 mt-10 w-5/6 h-20 bg-yellow-300">
+      <section className="flex justify-evenly gap-2 mt-10 w-5/6 h-20 ">
         <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
         <div className="bg-white w-1/12 rounded-md border-2 border-black"></div>
 
