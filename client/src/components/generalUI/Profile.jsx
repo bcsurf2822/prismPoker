8698989c6b916ca6bb362b3fe962ca38bb1264e9
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-02-20 11:35:25 -0500

    add animation for home page

--- client/src/components/generalUI/Profile.jsx
+++ client/src/components/generalUI/Profile.jsx
@@ -1,19 +1,25 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
   const user = useSelector((state) => state.auth.user);
-  console.log("User: ", user)
-  const dispatch = useDispatch();
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <div className="card-body">
         <h2 className="card-title">Profile</h2>
-        <p><span className="font-bold">Username: </span>{user.username}</p>
-        <p><span className="font-bold">Email: </span>{user.email}</p>
-        <p><span className="font-bold">SC: </span>{user.accountBalance}</p>
-        <div className="card-actions justify-end">
-
-        </div>
+        <p>
+          <span className="font-bold">Username: </span>
+          {user.username}
+        </p>
+        <p>
+          <span className="font-bold">Email: </span>
+          {user.email}
+        </p>
+        <p>
+          <span className="font-bold">SC: </span>
+          {user.accountBalance}
+        </p>
+        <div className="card-actions justify-end"></div>
       </div>
     </div>
   );
