c8d69bf8346b3e7cc65b8aa74dba06ad2491b010
ocneb <111916951+bcsurf2822@users.noreply.github.com>
2025-02-20 14:13:35 -0500

    add counter in the Account section

--- client/src/components/generalUI/Account.jsx
+++ client/src/components/generalUI/Account.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addFunds } from "../../features/auth/authenticationSlice";
+import Counter from "./Counter";
 
 export default function Account() {
   const user = useSelector((state) => state.auth.user);
@@ -11,7 +12,6 @@ export default function Account() {
 
   const handleInputChange = (e) => {
     const value = e.target.value;
-
     if (/^\d*\.?\d*$/.test(value)) {
       setAmount(value);
       setError("");
@@ -24,7 +24,6 @@ export default function Account() {
       setError("Please enter a valid amount greater than 0.");
       return;
     }
-
     dispatch(addFunds({ amount: numericAmount }))
       .unwrap()
       .then(() => {
@@ -37,34 +36,27 @@ export default function Account() {
   };
 
   return (
-    <div className="card card-side bg-base-100 shadow-xl">
-      <div className="card-body flex flex-col">
+    <div className="card card-side bg-base-100 shadow-xl p-4">
+      <div className="card-body flex flex-col gap-4">
         <h2 className="card-title font-bold text-center underline">Account</h2>
         <p>
-          {" "}
-          <span className="font-bold">Current Balance:</span> $
-          {user.accountBalance}
+          <span className="font-bold">Current Balance: $</span>
+          {user ? <Counter value={user.accountBalance} /> : "0"}
         </p>
-        <div className="card-actions justify-end">
-          <label className="form-control w-full max-w-xs">
-            <div className="label">
-              <span className="label-text font-semibold">
-                Enter Funds to Add
-              </span>
-            </div>
-            <input
-              type="text"
-              placeholder="$"
-              className="input input-bordered w-full max-w-xs"
-              value={amount}
-              onChange={handleInputChange}
-            />
-          </label>
-          {error && <p className="text-red-500">{error}</p>}
-          <button onClick={handleSubmit} className="btn btn-success">
-            Submit
-          </button>
-        </div>
+        <label className="form-control w-full max-w-xs">
+          <span className="label-text font-semibold">Enter Funds to Add</span>
+          <input
+            type="text"
+            placeholder="$"
+            className="input input-bordered w-full max-w-xs"
+            value={amount}
+            onChange={handleInputChange}
+          />
+        </label>
+        {error && <p className="text-red-500">{error}</p>}
+        <button onClick={handleSubmit} className="btn btn-success">
+          Submit
+        </button>
       </div>
     </div>
   );
